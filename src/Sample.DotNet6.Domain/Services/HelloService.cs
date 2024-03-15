97c2b15bbb8a3bb6f37780206e07ebf446df2528
Reuben Sammut <74065441+resa01@users.noreply.github.com>
2024-03-15 15:11:28 +0100

    Example using MD5

--- src/Sample.DotNet6.Domain/Services/HelloService.cs
+++ src/Sample.DotNet6.Domain/Services/HelloService.cs
@@ -7,9 +7,29 @@
 
     public class HelloService : IHelloService
     {
+        public static string CreateMD5(string input)
+        {
+            // Use input string to calculate MD5 hash
+            using (System.Security.Cryptography.MD5 md5 = System.Security.Cryptography.MD5.Create())
+            {
+                byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(input);
+                byte[] hashBytes = md5.ComputeHash(inputBytes);
+        
+                return Convert.ToHexString(hashBytes); // .NET 5 +
+        
+                // Convert the byte array to hexadecimal string prior to .NET 5
+                // StringBuilder sb = new System.Text.StringBuilder();
+                // for (int i = 0; i < hashBytes.Length; i++)
+                // {
+                //     sb.Append(hashBytes[i].ToString("X2"));
+                // }
+                // return sb.ToString();
+            }
+        }
         public string Hello(bool isHappy)
         {
             var hello = $"Hello";
+            hello = CreateMD5(hello);
 
             if (isHappy)
                 return $"{hello}, you seem to be happy today";
