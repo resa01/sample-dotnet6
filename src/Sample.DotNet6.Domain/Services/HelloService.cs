78de205684fdb83d4d6c98265d144722060ae658
Reuben Sammut <74065441+resa01@users.noreply.github.com>
2024-03-15 15:21:21 +0100

    Example - Added encryption

--- src/Sample.DotNet6.Domain/Services/HelloService.cs
+++ src/Sample.DotNet6.Domain/Services/HelloService.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Text;
+using System.Security.Cryptography;
 
 namespace Sample.DotNet6.Domain.Services
 {
@@ -10,6 +11,47 @@ namespace Sample.DotNet6.Domain.Services
 
     public class HelloService : IHelloService
     {
+        public static string Encrypt(string toEncrypt, bool useHashing)
+        {
+            byte[] keyArray;
+            byte[] toEncryptArray = UTF8Encoding.UTF8.GetBytes(toEncrypt);
+        
+            System.Configuration.AppSettingsReader settingsReader = new AppSettingsReader();
+            // Get the key from config file
+        
+            string key = (string)settingsReader.GetValue("SecurityKey", typeof(String));
+            //System.Windows.Forms.MessageBox.Show(key);
+            //If hashing use get hashcode regards to your key
+            if (useHashing)
+            {
+                MD5CryptoServiceProvider hashmd5 = new MD5CryptoServiceProvider();
+                keyArray = hashmd5.ComputeHash(UTF8Encoding.UTF8.GetBytes(key));
+                //Always release the resources and flush data
+                //of the Cryptographic service provide. Best Practice
+        
+                hashmd5.Clear();
+            }
+            else
+                keyArray = UTF8Encoding.UTF8.GetBytes(key);
+        
+            TripleDESCryptoServiceProvider tdes = new TripleDESCryptoServiceProvider();
+            //set the secret key for the tripleDES algorithm
+            tdes.Key = keyArray;
+            //mode of operation. there are other 4 modes. We choose ECB(Electronic code Book)
+            tdes.Mode = CipherMode.ECB;
+            //padding mode(if any extra byte added)
+            tdes.Padding = PaddingMode.PKCS7;
+        
+            ICryptoTransform cTransform = tdes.CreateEncryptor();
+            //transform the specified region of bytes array to resultArray
+            byte[] resultArray = cTransform.TransformFinalBlock
+                    (toEncryptArray, 0, toEncryptArray.Length);
+            //Release resources held by TripleDes Encryptor
+            tdes.Clear();
+            //Return the encrypted data into unreadable string format
+            return Convert.ToBase64String(resultArray, 0, resultArray.Length);
+        }
+        
         public static string CreateMD5(string input)
         {
             // Use input string to calculate MD5 hash
@@ -32,7 +74,7 @@ namespace Sample.DotNet6.Domain.Services
         public string Hello(bool isHappy)
         {
             var hello = $"Hello";
-            hello = CreateMD5(hello);
+            hello = Encrypt(hello, true);
 
             if (isHappy)
                 return $"{hello}, you seem to be happy today";
